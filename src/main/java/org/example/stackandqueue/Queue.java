98fcd9431bfdab483e42da828d81df1af3873688
kawinkumar25 <123533543+kawinkumar25@users.noreply.github.com>
2023-03-03 14:40:52 +0530

    Update Queue.java

--- src/main/java/org/example/stackandqueue/Queue.java
+++ src/main/java/org/example/stackandqueue/Queue.java
@@ -3,7 +3,7 @@ package org.example.stackandqueue;
 public class Queue {
 
     public void perfrom() {
-        queuemethods queue = new queuemethods();
+        Queuemethods queue = new Queuemethods();
         queue.addElement(12);
         queue.addElement(4);
         queue.addElement(10);
